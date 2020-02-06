a659d88c2093b84ffac21375049ba56d2913ce38
CPark <parkchaeah331@gmail.com>
2020-02-06 18:52:28 +0200

    Updates

---
+++ precache-manifest.bb4b50339561cf702ec77d92438c28df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a10806362a33defd2aba2be3649d479",
+    "url": "/restaurant-helsinki/index.html"
+  },
+  {
+    "revision": "9b5f70b6ed32196cb356",
+    "url": "/restaurant-helsinki/static/css/main.ab1f960b.chunk.css"
+  },
+  {
+    "revision": "b11b99f192158d1c6222",
+    "url": "/restaurant-helsinki/static/js/2.9894ba7f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/restaurant-helsinki/static/js/2.9894ba7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b5f70b6ed32196cb356",
+    "url": "/restaurant-helsinki/static/js/main.26616cd4.chunk.js"
+  },
+  {
+    "revision": "f6e1c7423a9f342eb2d7",
+    "url": "/restaurant-helsinki/static/js/runtime-main.9e45505d.js"
+  }
+]);
